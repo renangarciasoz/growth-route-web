399667d9d8e598cc1c82948c60aea1d365f9b0e2
Renan Garcia <renan.garcia@loft.com.br>
2020-02-20 00:57:16 -0800

    chore: remove custom server

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
-const cli = require("./cli");
-const getPublicRuntimeConfig = require("./config/public.config.js");
-const getServerRuntimeConfig = require("./config/server.config.js");
-const assertEnvironment = require("./config/assertEnvironment");
-
-assertEnvironment();
-
-const cliFlags = cli.parse(process.argv);
-const publicRuntimeConfig = getPublicRuntimeConfig(cliFlags);
-const serverRuntimeConfig = getServerRuntimeConfig(cliFlags);
-
 module.exports = {
-  publicRuntimeConfig,
-  serverRuntimeConfig
+  branch: "GrowthRoute",
+  email: "email@email.com",
+  telephone: "(11) 1234-5123",
 };
